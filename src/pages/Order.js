9a47d4e41f490ef62ac39699e02dd9acd9b9d89c
swarup <swarupanandagorai@hotmail.com>
2024-03-16 13:33:20 +0530

    statement option added

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -18,6 +18,8 @@ import moment from "moment";
 import BillPreview from "../components/Business/BillPreview";
 import { isLoggedIn } from "../store/auth-slice";
 import Stock from "../components/Business/Stock";
+import { Button } from "primereact/button";
+import { ToWords } from "to-words";
 
 export default function Order() {
   const columns = useSelector(orderHeaders);
@@ -36,6 +38,7 @@ export default function Order() {
   const [addStock, setAddStock] = useState(false);
   const isUserLoggedIn = useSelector(isLoggedIn);
   const [editingItem, setEditingItem] = useState({});
+  const [isStatement, setIsStatement] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -44,6 +47,26 @@ export default function Order() {
     loadTable();
     dispatch(fetchOrder());
   }, []);
+
+  const toWords = new ToWords({
+    localeCode: 'en-IN',
+    converterOptions: {
+      currency: true,
+      ignoreDecimal: false,
+      ignoreZeroCurrency: false,
+      doNotAddOnly: false,
+      currencyOptions: { // can be used to override defaults for the selected locale
+        name: 'Rupee',
+        plural: 'Rupees',
+        symbol: '₹',
+        fractionalUnit: {
+          name: 'Paisa',
+          plural: 'Paise',
+          symbol: '',
+        },
+      }
+    }
+  });
 
   const viewDetails = (data, index) => {
     console.log(data);
@@ -56,32 +79,38 @@ export default function Order() {
     console.log(data, index);
     setAddStock(true);
     setEditingItem(data);
-    document.body.classList.add('hidden-overflow')
+    document.body.classList.add("hidden-overflow");
   };
 
   const hideAddStockHandler = () => {
     setAddStock(false);
-    document.body.classList.remove('hidden-overflow')
+    document.body.classList.remove("hidden-overflow");
   };
 
   const updateOrder = async (updatingItem) => {
     await dispatch(putOrder(updatingItem));
     setAddStock(false);
-    document.body.classList.remove('hidden-overflow');
+    document.body.classList.remove("hidden-overflow");
     dispatch(fetchOrder());
-  }
+  };
 
-  const totalNetLeaf = orders.reduce((carry, item) => {
-    return item.netLeafKgs ? carry + parseFloat(item.netLeafKgs) : carry + 0;
-  }, 0).toFixed(2);
+  const totalNetLeaf = orders
+    .reduce((carry, item) => {
+      return item.netLeafKgs ? carry + parseFloat(item.netLeafKgs) : carry + 0;
+    }, 0)
+    .toFixed(2);
   const totalDebitAmount = orders
     .reduce((carry, item) => {
-      return item.debitAmount > 0 ? carry + parseFloat(item.debitAmount) : carry + 0;
+      return item.debitAmount > 0
+        ? carry + parseFloat(item.debitAmount)
+        : carry + 0;
     }, 0)
     .toFixed(2);
   const totalCreditAmount = orders
     .reduce((carry, item) => {
-      return item.creditAmount > 0 ? carry + parseFloat(item.creditAmount) : carry + 0;
+      return item.creditAmount > 0
+        ? carry + parseFloat(item.creditAmount)
+        : carry + 0;
     }, 0)
     .toFixed(2);
 
@@ -118,6 +147,7 @@ export default function Order() {
   const resetSearch = async () => {
     setFromDate();
     setToDate();
+    setIsStatement(false);
     // setSelectedCustomer();
     setRecordSearched(false);
     await dispatch(fetchOrder());
@@ -131,15 +161,6 @@ export default function Order() {
         customer: selectedCustomer,
       })
     );
-    if (orders.length) {
-      setRecordSearched(true);
-      dispatch(orderActions.getBillingItems());
-    }
-  };
-
-  const creatingBill = () => {
-    document.body.classList.add("hidden-overflow");
-    setPreviewBill(true);
     setTitle(
       selectedCustomer
         ? `${moment(fromDate).format("YYYY-MM-DD")} - ${moment(toDate).format(
@@ -149,7 +170,125 @@ export default function Order() {
             "YYYY-MM-DD"
           )}`
     );
+    if (orders.length) {
+      setRecordSearched(true);
+      dispatch(orderActions.getBillingItems());
+    }
+    console.log(selectedCustomer)
+    setIsStatement(selectedCustomer ? true : false);
+  };
+
+  const creatingBill = () => {
+    document.body.classList.add("hidden-overflow");
+    setPreviewBill(true);
     resetSearch();
+  };
+
+  const pdfStatement = () => {
+      import("jspdf").then((jsPDF) => {
+        import("jspdf-autotable").then(() => {
+          const exportColumns = [...columns]
+          .map((col) => ({
+            title: col.header,
+            dataKey: col.field,
+          }))
+          .filter((i) => i.dataKey);
+          const doc = new jsPDF.default({ orientation: "l" }, 0, 0);
+          doc.setFont('Times New Roman');
+          doc.text("STATEMENT", 135, 10);
+          doc.text(`PARTY NAME: ${selectedCustomer?.name}`, 97, 20);
+          doc.text(`PERIOD: FROM ${moment(fromDate).format("YYYY-MM-DD")} TO ${moment(toDate).format(
+            "YYYY-MM-DD"
+          )}`, 100, 30);
+          let position = 0;
+          const outstanding = totalCreditAmount > totalDebitAmount ? (totalCreditAmount - totalDebitAmount).toFixed(2) : 0;
+          const otherCol = [
+            {
+              title: "VCH No",
+              dataKey: "vchNo",
+            }
+          ];
+          const pdfColumn = [...exportColumns];
+          pdfColumn.splice(6, 0, ...otherCol);
+          const final = [
+            {
+              transactionDate: "",
+              invoiceNo: "",
+              type: "",
+              netLeafKgs: "",
+              rateKg: "",
+              "customerId.name": "",
+              debitAmount: "",
+              creditAmount: "",
+              vchNo: "",
+              clNo: "",
+              qlty: "",
+              note: ``,
+            },
+            {
+              transactionDate: "",
+              invoiceNo: "",
+              type: "",
+              netLeafKgs: totalNetLeaf,
+              rateKg: "",
+              "customerId.name": "Total",
+              debitAmount: totalDebitAmount,
+              creditAmount: totalCreditAmount,
+              vchNo: "",
+              clNo: "",
+              qlty: "",
+              note: `Outstanding: ${outstanding}`,
+            },
+          ];
+  
+          const pdfData = orders
+            .map((d) => ({
+              transactionDate: moment(d.transactionDate).utc().format("DD/MM/YYYY"),
+              invoiceNo: d.invoiceNo,
+              type: d.type,
+              netLeafKgs: d.netLeafKgs,
+              rateKg: d.rateKg > 0 ? parseFloat(d.rateKg).toFixed(2) : d.rateKg,
+              "customerId.name": d.customerId.name,
+              debitAmount:
+                d.debitAmount > 0
+                  ? parseFloat(d.debitAmount).toFixed(2)
+                  : d.debitAmount,
+              creditAmount:
+                d.creditAmount > 0
+                  ? parseFloat(d.creditAmount).toFixed(2)
+                  : d.creditAmount,
+              vchNo: d.vchNo,
+              clNo: d.clNo,
+              qlty: d.qlty,
+              note: d.note,
+            }))
+            .filter((i) => i);
+          doc.autoTable(
+            [
+              ...pdfColumn,
+              {
+                title: "Note",
+                dataKey: "note",
+              },
+            ],
+            [...pdfData, ...final],
+            {
+              margin: { top: 35 },
+              didDrawPage: (d) =>  {
+                console.log('possition',d.cursor.y)
+                position = d.cursor.y + 7;
+                d.settings.margin.top = 10;
+              }
+            }
+          );
+          doc.text(`Total Credit Amount in Words: ${toWords.convert(totalCreditAmount)}.`, 10, position);
+          position = position + 10;
+          doc.text(`Total Debit Amount in Words: ${toWords.convert(totalDebitAmount)}.`, 10, position);
+          position = position + 10;
+          doc.text(`Total Outstanding Amount in Words: ${outstanding > 0 ? toWords.convert(outstanding) : 'No Outstanding Amount'}.`, 10, position);
+          doc.save(`${title || "invoices"}.pdf`);
+        });
+      });
   };
 
   const closePreviewBill = () => {
@@ -160,7 +299,7 @@ export default function Order() {
   const dateSelect = (
     <div className={`card-body search-body`}>
       <div className="row">
-        <div className="col-md-5">
+        <div className="col-md-4">
           <Calendar
             value={fromDate}
             onChange={(e) => setFromDate(e.value)}
@@ -197,27 +336,37 @@ export default function Order() {
             style={{ flex: "3.1 1" }}
           />
         </div>
-        <div className="col-md-3" style={{ float: "left" }}>
-          <button
+        <div
+          className="col-md-5"
+          style={{ display: "flex", justifyContent: "flex-start" }}
+        >
+          <Button
+            label="Search"
             type="button"
             className="p-button p-button-danger p-button-rounded"
             onClick={filterRecord}
-          >
-            Search Record
-          </button>
+          />
           <i
             className="pi pi-replay"
             onClick={resetSearch}
             style={{ fontSize: "2rem", margin: "8px", cursor: "pointer" }}
           ></i>
           {recordSearched && (
-            <button
+            <Button
               type="button"
+              label="Create Bill"
               className="p-button p-button-rounded"
               onClick={creatingBill}
-            >
-              Create Bill
-            </button>
+            />
+          )}
+          {isStatement && (
+            <Button
+              type="button"
+              label="Statement"
+              className="p-button p-button-rounded ms-2"
+              onClick={pdfStatement}
+              severity="help"
+            />
           )}
         </div>
       </div>
@@ -226,7 +375,13 @@ export default function Order() {
 
   return (
     <Fragment>
-      {addStock && isUserLoggedIn && <Stock onClose={hideAddStockHandler} editingItem={editingItem} updateOrder={updateOrder}/>}
+      {addStock && isUserLoggedIn && (
+        <Stock
+          onClose={hideAddStockHandler}
+          editingItem={editingItem}
+          updateOrder={updateOrder}
+        />
+      )}
       <div className={`card overlay`}>
         <div className="card-header text-white">Sales List</div>
         {preview && (
@@ -255,16 +410,18 @@ export default function Order() {
           editItem={editItem}
           show={true}
           edit={true}
-          filetext={selectedCustomer
-        ? `${moment(fromDate).format("YYYY-MM-DD")} - ${moment(toDate).format(
-            "YYYY-MM-DD"
-          )} | ${selectedCustomer.name}`
-        : `${moment(fromDate).format("YYYY-MM-DD")} - ${moment(toDate).format(
-            "YYYY-MM-DD"
-          )}`}
+          filetext={
+            selectedCustomer
+              ? `${moment(fromDate).format("YYYY-MM-DD")} - ${moment(
+                  toDate
+                ).format("YYYY-MM-DD")} | ${selectedCustomer.name}`
+              : `${moment(fromDate).format("YYYY-MM-DD")} - ${moment(
+                  toDate
+                ).format("YYYY-MM-DD")}`
+          }
         />
         <div className="text-white">
-          <table style={{ width: "100%"}}>
+          <table style={{ width: "100%" }}>
             <tr>
               <td>Total Net Leaf: {totalNetLeaf}</td>
               <td>Total Debit Amount: {totalDebitAmount}</td>
